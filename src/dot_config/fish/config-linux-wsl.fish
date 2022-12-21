b3009fd4b7633beb664c0eca33c1c82c2bae7d36
ispern <h.matsuoka@wiz-net.jp>
2022-12-21 07:39:15 +0000

    bug(fish): Delete files in fisher management.

--- src/dot_config/fish/config-linux-wsl.fish
+++ src/dot_config/fish/config-linux-wsl.fish
@@ -1,30 +1,31 @@
 set -gx WIN_HOME /mnt/c/Users/h.matsuoka
 
 # Docker for Windows
-set -gx PATH /mnt/c/Program Files/Docker/Docker/resources/bin:/mnt/c/ProgramData/DockerDesktop/version-bin $PATH
+fish_add_path /mnt/c/Program Files/Docker/Docker/resources/bin:/mnt/c/ProgramData/DockerDesktop/version-bin
 
 # Windows Default Application
 alias open "/mnt/c/Windows/explorer.exe"
 
 # Google chorome
-set -gx PATH "/mnt/c/Program Files/Google/Chrome/Application" $PATH
+fish_add_path "/mnt/c/Program Files/Google/Chrome/Application"
 set -gx LD_LIBRARY_PATH $LD_LIBRARY_PATH /lib/x86_64-linux-gnu
 set -gx LD_LIBRARY_PATH $LD_LIBRARY_PATH /usr/lib/x86_64-linux-gnu
 set -gx BROWSER chrome.exe
 
 # VSCode
-set -gx PATH "$WIN_HOME/AppData/Local/Programs/Microsoft VS Code/bin" $PATH
+fish_add_path "$WIN_HOME/AppData/Local/Programs/Microsoft VS Code/bin"
 
 # NeoVim
 set -gx NEOVIM_WIN_DIR "/mnt/c/Program Files/Neovim/bin/"
-set -gx PATH $NEOVIM_WIN_DIR $PATH
+fish_add_path $NEOVIM_WIN_DIR
 
-# Linuxbrew
-if test -d /home/linuxbrew/.linuxbrew/
-  eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+if test -d /home/linuxbrew/.linuxbrew/; or test -d $HOME/.linuxbrew
+  if status --is-interactive
+    eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+  end
 
   # diff-highlight
-  set -gx PATH /home/linuxbrew/.linuxbrew/Cellar/git/2.38.0/share/git-core/contrib/diff-highlight/ $PATH
+  fish_add_path /home/linuxbrew/.linuxbrew/Cellar/git/2.38.0/share/git-core/contrib/diff-highlight/
 end
 
 # Starship
@@ -41,5 +42,5 @@ end
 # volta
 if test -d $HOME/.volta
   set -gx VOLTA_HOME $HOME/.volta
-  set -gx PATH $VOLTA_HOME/bin $PATH
+  fish_add_path $VOLTA_HOME/bin
 end
--- src/dot_config/fish/config-linux.fish
+++ src/dot_config/fish/config-linux.fish
@@ -1,3 +1,10 @@
 # Global
 #set -gx TERM "xterm-256color"
 #set -gx COLORTERM "truecolor"
+if test -d "$HOME/.local/bin"
+  set -gx PATH "$HOME/bin" $PATH
+end
+
+if test -d "$HOME/.local/bin"
+  set -gx PATH "$HOME/.local/bin" $PATH
+end
