03419f92e1d7201acf923f3f367ca14616a2053c
ispern <h.matsuoka@wiz-net.jp>
2023-01-15 07:19:45 +0000

    chore(neovim): modify shortcut.

--- src/dot_config/nvim/lua/config/telescope_setup.lua
+++ src/dot_config/nvim/lua/config/telescope_setup.lua
@@ -6,6 +6,6 @@ local silent = { silent = true, noremap = true }
 map('n', '<c-e>', [[<cmd>Telescope buffers show_all_buffers=true theme=get_dropdown<cr>]], silent)
 map('n', '<c-p>', [[<cmd>Telescope commands theme=get_dropdown<cr>]], silent)
 map('n', '<c-s>', [[<cmd>Telescope git_files theme=get_dropdown<cr>]], silent)
-map('n', '<c-o>', [[<cmd>Telescope find_files theme=get_dropdown hidden=true<cr>]], silent)
+map('n', '<c-s-o>', [[<cmd>Telescope find_files theme=get_dropdown hidden=true<cr>]], silent)
 map('n', '<c-s-F>', [[<cmd>Telescope live_grep theme=get_dropdown hidden=true<cr>]], silent)
 map('n', '<a-2>', [[<cmd>Telescope file_browser hidden=true<cr>]], silent)
