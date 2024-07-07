3d4c89faaf9f8f67e4dae559feda1bec371553b9
ispern <h.matsuoka@wiz-net.jp>
2024-07-07 02:23:40 +0000

    change telescope key mappings.

--- src/dot_config/nvim/lua/config/telescope_setup.lua
+++ src/dot_config/nvim/lua/config/telescope_setup.lua
@@ -3,9 +3,9 @@ local map = vim.api.nvim_set_keymap
 local silent = { silent = true, noremap = true }
 
 -- Navigate buffers and repos
-map('n', '<c-e>', [[<cmd>Telescope buffers show_all_buffers=true theme=get_dropdown<cr>]], silent)
+map('n', '<Leader>tb', [[<cmd>Telescope buffers show_all_buffers=true theme=get_dropdown<cr>]], silent)
 map('n', '<c-p>', [[<cmd>Telescope commands theme=get_dropdown<cr>]], silent)
-map('n', '<c-s>', [[<cmd>Telescope git_files theme=get_dropdown<cr>]], silent)
-map('n', '<c-s-o>', [[<cmd>Telescope find_files theme=get_dropdown hidden=true<cr>]], silent)
-map('n', '<c-s-F>', [[<cmd>Telescope live_grep theme=get_dropdown hidden=true<cr>]], silent)
+map('n', '<Leader>tgs', [[<cmd>Telescope git_status theme=get_dropdown<cr>]], silent)
+map('n', '<Leader>tf', [[<cmd>Telescope find_files theme=get_dropdown hidden=true<cr>]], silent)
+map('n', '<Leader>tl', [[<cmd>Telescope live_grep theme=get_dropdown hidden=true<cr>]], silent)
 map('n', '<a-2>', [[<cmd>Telescope file_browser hidden=true<cr>]], silent)
