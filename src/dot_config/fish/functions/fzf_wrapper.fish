ef9829021d7dcc455f6e6d51c0fb7df06a8c2511
ispern <h.matsuoka@wiz-net.jp>
2024-09-22 06:41:56 +0000

    fzf-tmxが動作しないので一時的に無効化

--- src/dot_config/fish/functions/fzf_wrapper.fish
+++ src/dot_config/fish/functions/fzf_wrapper.fish
@@ -3,13 +3,13 @@ function fzf_wrapper --description "Prepares some environment variables before e
 
     # https://github.com/junegunn/fzf#environment-variables
     if not set --query FZF_DEFAULT_OPTS
-      set -gx FZF_DEFAULT_OPTS '--cycle --ansi --layout=reverse --border --height=50% --preview-window=wrap --marker="*"'
+        set -gx FZF_DEFAULT_OPTS '--cycle --ansi --layout=reverse --border --height=50% --preview-window=wrap --marker="*"'
     end
 
-    if not set --query TMUX
-      fzf $argv
-    else
-      # FZF_TMUX_OTPS is not working?
-      fzf-tmux -p 80% $argv
-    end
+    # if not set --query TMUX
+    fzf $argv
+    # else
+    # FZF_TMUX_OTPS is not working?
+    # fzf-tmux -p 80% $argv
+    # end
 end
