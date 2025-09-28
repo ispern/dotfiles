d5253dcf0e153511595544b57e5c8ae4a30097dd
ispern <h.matsuoka@wiz-net.jp>
2025-09-28 21:25:31 +0900

    add support mac.

---
+++ src/dot_config/fish/config-osx.fish
@@ -0,0 +1,66 @@
+# macOS specific configuration for Fish shell
+
+# Homebrew setup for Apple Silicon
+if test -d /opt/homebrew
+    set -gx HOMEBREW_PREFIX /opt/homebrew
+    set -gx HOMEBREW_CELLAR /opt/homebrew/Cellar
+    set -gx HOMEBREW_REPOSITORY /opt/homebrew
+    fish_add_path -gP /opt/homebrew/bin /opt/homebrew/sbin
+    set -q MANPATH; or set MANPATH ''
+    set -gx MANPATH /opt/homebrew/share/man $MANPATH
+    set -q INFOPATH; or set INFOPATH ''
+    set -gx INFOPATH /opt/homebrew/share/info $INFOPATH
+end
+
+# GNU tools from Homebrew
+if test -d /opt/homebrew/opt/coreutils/libexec/gnubin
+    fish_add_path -gP /opt/homebrew/opt/coreutils/libexec/gnubin
+end
+if test -d /opt/homebrew/opt/gnu-sed/libexec/gnubin
+    fish_add_path -gP /opt/homebrew/opt/gnu-sed/libexec/gnubin
+end
+if test -d /opt/homebrew/opt/findutils/libexec/gnubin
+    fish_add_path -gP /opt/homebrew/opt/findutils/libexec/gnubin
+end
+if test -d /opt/homebrew/opt/grep/libexec/gnubin
+    fish_add_path -gP /opt/homebrew/opt/grep/libexec/gnubin
+end
+
+# Environment variables
+set -gx EDITOR nvim
+set -gx VISUAL nvim
+set -gx PAGER less
+
+# macOS specific aliases
+alias ls "eza --icons"
+alias ll "eza --icons -la"
+alias la "eza --icons -a"
+alias lt "eza --icons --tree"
+alias cat "bat"
+alias top "htop"
+
+# Remove .DS_Store files recursively
+alias rm_ds "find . -name '.DS_Store' -type f -delete"
+
+# Quick Look from terminal
+alias ql "qlmanage -p 2>/dev/null"
+
+# Clipboard aliases
+alias pbcopy "pbcopy"
+alias pbpaste "pbpaste"
+
+# 1Password SSH Agent setup (if available)
+if test -S "$HOME/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
+    set -gx SSH_AUTH_SOCK "$HOME/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
+end
+
+# FZF configuration
+if command -v fzf >/dev/null
+    set -gx FZF_DEFAULT_COMMAND 'rg --files --hidden --follow --glob "!.git/*"'
+    set -gx FZF_DEFAULT_OPTS '--height 40% --reverse --border'
+end
+
+# Starship prompt
+if command -v starship >/dev/null
+    starship init fish | source
+end
--- src/dot_zsh/os/darwin.zsh
+++ src/dot_zsh/os/darwin.zsh
@@ -1,67 +1,90 @@
-# Global
-export ZPLUG_HOME=/usr/local/opt/zplug
+# macOS specific configuration for Zsh
+
+# Environment variables
 export TERM="xterm-256color"
-export PATH=/usr/local/bin:/usr/local/sbin:/usr/local/bin:~/bin/:$PATH
-export PATH=$HOME/Library/Python/3.7/bin/:$PATH
-export PATH=/usr/local/opt/coreutils/libexec/gnubin:$PATH
-export SHELL=/usr/local/bin/zsh
-
+export SHELL=/opt/homebrew/bin/fish
 export XDG_CONFIG_HOME=$HOME/.config
+export EDITOR=nvim
+export VISUAL=nvim
+export PAGER=less
 
-# Java
-export JAVA_OPTS="-server -Xms1024M -Xmx2048M -Xss3m"
-export _JAVA_OPTIONS="-Xms1024M -Xmx2048M -Xss3m -Dfile.encoding=UTF-8"
-export JAVA_OPTS="-server -Xms1024M -Xmx2048M -Xss3m"
-export _JAVA_OPTIONS="-Xms1024M -Xmx2048M -Xss3m -Dfile.encoding=UTF-8"
-export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
-export SBT_OPTS="-server -Xms1024M -Xmx2048M -Xss3m"
-
-# nodebrew
-if [ -e $HOME/.nodebrew ]; then
-    export PATH=$HOME/.nodebrew/current/bin:$PATH
-    nodebrew use v10.16.0
+# Homebrew setup for Apple Silicon
+if [ -d "/opt/homebrew" ]; then
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+    export PATH="/opt/homebrew/bin:/opt/homebrew/sbin:$PATH"
 fi
 
-# rbenv
-if [ -e $HOME/.rbenv ]; then
-    export PATH=$HOME/.rbenv/bin:$PATH
-    export PATH=$HOME/.rbenv/shims:$PATH
+# GNU tools from Homebrew
+if [ -d "/opt/homebrew/opt/coreutils/libexec/gnubin" ]; then
+    export PATH="/opt/homebrew/opt/coreutils/libexec/gnubin:$PATH"
+fi
+if [ -d "/opt/homebrew/opt/gnu-sed/libexec/gnubin" ]; then
+    export PATH="/opt/homebrew/opt/gnu-sed/libexec/gnubin:$PATH"
+fi
+if [ -d "/opt/homebrew/opt/findutils/libexec/gnubin" ]; then
+    export PATH="/opt/homebrew/opt/findutils/libexec/gnubin:$PATH"
+fi
+if [ -d "/opt/homebrew/opt/grep/libexec/gnubin" ]; then
+    export PATH="/opt/homebrew/opt/grep/libexec/gnubin:$PATH"
+fi
+
+# User binaries
+export PATH="$HOME/bin:$HOME/.local/bin:$PATH"
+
+# FZF configuration
+if command -v fzf &> /dev/null; then
+    export FZF_DEFAULT_COMMAND='rg --files --hidden --follow --glob "!.git/*"'
+    export FZF_DEFAULT_OPTS='--height 40% --reverse --border'
+fi
+
+# 1Password SSH Agent setup (if available)
+if [ -S "$HOME/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock" ]; then
+    export SSH_AUTH_SOCK="$HOME/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
+fi
+
+# Starship prompt
+if command -v starship &> /dev/null; then
+    eval "$(starship init zsh)"
+fi
+
+# rbenv setup (if installed)
+if [ -d "$HOME/.rbenv" ]; then
+    export PATH="$HOME/.rbenv/bin:$PATH"
     eval "$(rbenv init - zsh)"
 fi
 
-export STARSHIP_CONFIG=~/.dotfiles/starship.toml
+# nodenv setup (if installed)
+if [ -d "$HOME/.nodenv" ]; then
+    export PATH="$HOME/.nodenv/bin:$PATH"
+    eval "$(nodenv init -)"
+fi
 
-eval "$(starship init zsh)"
+# pyenv setup (if installed)
+if [ -d "$HOME/.pyenv" ]; then
+    export PYENV_ROOT="$HOME/.pyenv"
+    export PATH="$PYENV_ROOT/bin:$PATH"
+    eval "$(pyenv init --path)"
+    eval "$(pyenv init -)"
+fi
 
-# anyframe
-zstyle ":anyframe:selector:" use fzf-tmux
+# Aliases
+alias ls='eza --icons'
+alias ll='eza --icons -la'
+alias la='eza --icons -a'
+alias lt='eza --icons --tree'
+alias cat='bat'
+alias top='htop'
+alias grep='grep --color=auto'
 
-# ghq
-source /usr/local/share/zsh/site-functions
+# macOS specific aliases
+alias rm_ds="find . -name '.DS_Store' -type f -delete"
+alias ql="qlmanage -p 2>/dev/null"
 
-# alias
-alias brew="env PATH=${PATH/\/Users\/hiromitsu\/\.phpenv\/shims:/} brew"
-alias rm_ds="find . -name '.DS_Store' -print -exec rm -r {} ';' ; find . -name ._* -exec rm -r {} ";
+# Clipboard aliases
+alias pbcopy='pbcopy'
+alias pbpaste='pbpaste'
 
-# hyper
-# Override auto-title when static titles are desired ($ title My new title)
-title() { export TITLE_OVERRIDDEN=1; echo -en "\e]0;$*\a"}
-# Turn off static titles ($ autotitle)
-autotitle() { export TITLE_OVERRIDDEN=0 }; autotitle
-# Condition checking if title is overridden
-overridden() { [[ $TITLE_OVERRIDDEN == 1 ]]; }
-# Echo asterisk if git state is dirty
-gitDirty() { [[ $(git status 2> /dev/null | grep -o '\w\+' | tail -n1) != ("clean"|"") ]] && echo "*" }
-
-# Show cwd when shell prompts for input.
-precmd() {
-   if overridden; then return; fi
-   cwd=${$(pwd)##*/} # Extract current working dir only
-   print -Pn "\e]0;$cwd$(gitDirty)\a" # Replace with $pwd to show full path
-}
-
-# Prepend command (w/o arguments) to cwd while waiting for command to complete.
-preexec() {
-   if overridden; then return; fi
-   printf "\033]0;%s\a" "${1%% *} | $cwd$(gitDirty)" # Omit construct from $1 to show args
-}
+# anyframe settings for fzf integration
+if command -v fzf-tmux &> /dev/null; then
+    zstyle ":anyframe:selector:" use fzf-tmux
+fi
