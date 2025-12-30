cfdde2e1874770a7974773595edcb8f1c829e89f
ispern <h.matsuoka@wiz-net.jp>
2025-12-30 21:35:13 +0900

    add wezterm.

---
+++ src/dot_config/wezterm/wezterm.lua
@@ -0,0 +1,91 @@
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+
+config.automatically_reload_config = true
+config.font = wezterm.font("UDEV Gothic NF", {
+    weight = "Medium",
+})
+config.font_size = 15.0
+config.use_ime = true
+config.window_background_opacity = 0.80
+config.macos_window_background_blur = 10
+
+----------------------------------------------------
+-- Colors
+----------------------------------------------------
+config.color_scheme = "MyTheme"
+config.window_frame = {
+  inactive_titlebar_bg = "none",
+  active_titlebar_bg = "none",
+}
+config.window_background_gradient = {
+  colors = { "#0B0D0F" },
+}
+
+----------------------------------------------------
+-- Tab
+----------------------------------------------------
+-- タイトルバーを非表示
+config.window_decorations = "RESIZE"
+-- タブバーの表示
+config.show_tabs_in_tab_bar = true
+-- タブが一つの時は非表示
+config.hide_tab_bar_if_only_one_tab = true
+-- falseにするとタブバーの透過が効かなくなる
+-- config.use_fancy_tab_bar = false
+
+-- タブバーの透過
+config.window_frame = {
+  inactive_titlebar_bg = "none",
+  active_titlebar_bg = "none",
+}
+
+-- タブの追加ボタンを非表示
+config.show_new_tab_button_in_tab_bar = false
+-- nightlyのみ使用可能
+-- タブの閉じるボタンを非表示
+config.show_close_tab_button_in_tabs = false
+-- タブの境界線を非表示
+config.colors = {
+  tab_bar = {
+    inactive_tab_edge = "none",
+  },
+}
+
+-- タブの形をカスタマイズ
+-- タブの左側の装飾
+local SOLID_LEFT_ARROW = wezterm.nerdfonts.ple_lower_right_triangle
+-- タブの右側の装飾
+local SOLID_RIGHT_ARROW = wezterm.nerdfonts.ple_upper_left_triangle
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+  local background = "#0B0D0F"
+  local foreground = "#F8F8F2"
+  local edge_background = "none"
+  if tab.is_active then
+    background = "#708CA9"
+    foreground = "#F8F8F2"
+  end
+  local edge_foreground = background
+  local title = "   " .. wezterm.truncate_right(tab.active_pane.title, max_width - 1) .. "   "
+  return {
+     { Background = { Color = edge_background } },
+     { Foreground = { Color = edge_foreground } },
+     { Text = SOLID_LEFT_ARROW },
+     { Background = { Color = background } },
+     { Foreground = { Color = foreground } },
+     { Text = title },
+     { Background = { Color = edge_background } },
+     { Foreground = { Color = edge_foreground } },
+     { Text = SOLID_RIGHT_ARROW },
+  }
+end)
+
+----------------------------------------------------
+-- keybinds
+----------------------------------------------------
+config.disable_default_key_bindings = true
+config.keys = require("keybindings").keys
+config.key_tables = require("keybindings").key_tables
+config.leader = { key = "g", mods = "CTRL", timeout_milliseconds = 2000 }
+
+return config
